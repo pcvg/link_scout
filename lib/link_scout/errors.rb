3eb155ec2a2781864b90980d488ced4a7f0c4f9c
Mike Peuerböck <mike@jetscale.com>
2018-04-04 12:05:14 +0200

    errors cleanup

--- lib/link_scout/errors.rb
+++ lib/link_scout/errors.rb
@@ -1,5 +1,7 @@
 module LinkScout
   # Raised whenever the given params dont match the expected input.
   class InvalidUsageError < RuntimeError; end
+
+  # Raised on Redirect Loops
   class RedirectLoopError < ArgumentError; end
 end
